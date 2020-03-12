f207354d19678a103a73f0830337baa28f22d999
chenxiaoling <chenxiaoling@sunseaaiot.com>
2020-03-12 15:53:36 +0800

    new prj

---
+++ src/main/java/com/cxl/toolproj/ToolprojApplication.java
@@ -0,0 +1,13 @@
+package com.cxl.toolproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxl/toolproj/ToolprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxl.toolproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
